77f79016ffa115df882125c0df675724848da1f9
MorenoAlexander <amoren26@uic.edu>
2020-07-11 23:12:47 -0500

    Add project files.

---
+++ DjangoWebEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\am171\source\repos\SelfLearning_DjangoWebProject\DjangoWebEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ SelfLearning_DjangoWebProject/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for SelfLearning_DjangoWebProject.
+"""
